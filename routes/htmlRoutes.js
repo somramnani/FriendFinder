003d006eaab025677409a62702b11c3eebb1922a
Som Ramnani <som.ramnani@gmail.com>
2019-06-17 14:40:01 -0500

    Added header, background images, and survey

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,31 @@
-var db = require("../models");
+// ===============================================================================
+// DEPENDENCIES
+// ===============================================================================
+  var path = require("path");
+  var db = require("../models");
+// ===============================================================================
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    // Load index page
+    app.get("/", function(req, res) {
+      res.sendFile(path.join(__dirname, "..public/index.html"));
+            
     });
-  });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+    app.get("/survey", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/survey.html"));
+            
     });
-  });
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+      res.render("404");
+    });
+  };
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
+// ===============================================================================

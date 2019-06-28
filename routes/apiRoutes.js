ebd9b81bb8bb43710a0dade63cc7323639eb66f4
Som Ramnani <som.ramnani@gmail.com>
2019-06-28 17:49:55 -0500

    edited the APIROtues file to make everything function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,68 +1,48 @@
-var db = require("../models");
+
 var friends = require("../data/friends")
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-  
-  app.get("/api/friends", function(req,res){
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req,res){
-    var user = req.body;
-    for(var i = 0; i < user.scores.length; i++){
-      user.scores[i] = parseInt(user.scores[i]);
-    }
+  app.post("/api/friends", function (req, res) {
+    var userData = req.body;
 
-    var bestFriendStartRange = 0;
+    var scores = [
+      parseInt(userData.question1),
+      parseInt(userData.question2),
+      parseInt(userData.question3),
+      parseInt(userData.question4),
+      parseInt(userData.question5),
+      parseInt(userData.question6),
+      parseInt(userData.question7),
+      parseInt(userData.question8),
+      parseInt(userData.question9),
+      parseInt(userData.question10)
+    ]
+
+    var bestFriendIndex = 0;
     var minimumDifference = 40;
 
-    for(var i = 0; i < friends.length; i++){
+    for (var i = 0; i < friends.length; i++) {
       var totalDifference = 0;
-
-      for(var j = 0; j < friends[i].scores.length; j++){
-        var difference = Math.abs(user.scores[j] - friends[i.scores[j]]);
-        totalDifference += difference; 
+      for (var j = 0; j < friends[i].scores.length; j++) {
+        
+        var difference = Math.abs(scores[j] - friends[i].scores[j]);
+        totalDifference += difference;
       }
 
-      if(totalDifference < minimumDifference){
-        bestFriendStartRange = i;
+      if (totalDifference < minimumDifference) {
+        bestFriendIndex = i;
         minimumDifference = totalDifference;
+
       }
+
+
     }
-
-    friends.push(user);
-    res.json(friends[bestFriendStartRange]);
-    
-
-
-
+    friends.push(userData);
+    res.json(friends[bestFriendIndex]);
 
   })
-
-
-
-
-
-
-
 };
